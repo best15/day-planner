9b92b96b0c8a907f26dfc60199577e68019d3bc1
SBC <lochan.sharad@gmail.com>
2021-04-08 23:06:35 +0800

    code added to create elements on page load and append hour through momentjs in the element

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,19 @@
 //Current day
 var gradDate = moment().format("dddd,MMMM Do");
 $("#currentDay").text(gradDate);
+
+
+for (var i = 0; i<12; i++ )
+{ 
+
+var nexthour = moment(String(starthour + i) + "AM", ["HH"]).format("hA");
+
+var timeblock = $("<div class = 'row time-block'></div>");
+var hour = $("<div class = 'hour'> </div>").text(nexthour);
+var textarea = $("<textarea>");
+var savebtn = $("<button  class = 'saveBtn'> <i class='bi bi-save2'></i> </button>");
+
+timeblock.append(hour,textarea,savebtn);
+$(".container").append(timeblock);
+
+}
